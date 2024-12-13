b5e7488573da658aca385dd06bb9923650fa3abe
pooji <poojithashree777@gmail.com>
2024-12-14 02:04:49 +0530

    added

--- code1.py
+++ code1.py
@@ -1,15 +1,1 @@
-def fib(n):
-    result=0
-    for i in range(n+1):
-        result+=i
-    return result
-
-res=fib(5)
-print(res)
-print("me")
-def printhello(name):
-    print(f"Hello{name})
-    print("me")
-printhello("pooji")
-
-print("Hi added")
+print("hello")
